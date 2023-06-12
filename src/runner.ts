8ef634795ab710d01d87ca1ce6ed568c0adba8aa
colmpat <cplang@dons.usfca.edu>
2023-06-12 09:58:59 -0700

    feat: add DateTime inference from github context

--- src/runner.ts
+++ src/runner.ts
@@ -1,4 +1,10 @@
 import * as core from '@actions/core'
+import * as github from '@actions/github'
+
+import { DateTime, Settings } from 'luxon'
+const zone = 'America/Los_Angeles';
+const eod = 'T23:59:59';
+Settings.defaultZone = zone;
 
 class Runner {
   score = ''
@@ -17,6 +23,9 @@ class Runner {
     core.info(`Score: ${this.score}`)
     core.info(`Points earned: ${this.pointsEarned}`)
     core.info(`Points possible: ${this.pointsPossible}`)
+
+    const submissionDate = this.parseSubmissionDate()
+    this.checkSubmissionDate(submissionDate)
   }
 
   /**
@@ -34,8 +43,52 @@ class Runner {
         let [earnedStr, possibleStr] = fields
         this.pointsEarned = parseInt(earnedStr)
         this.pointsPossible = parseInt(possibleStr)
+      } else {
+        throw new Error(`Score input is not in the correct format. Expected format: 80/100. Actual: ${score}`)
       }
     }
+  }
+
+  /**
+   * Parses the submission date from the GitHub Actions environment
+   * @returns The submission date
+   * @throws An error if the submission date is not in the correct format
+   */
+  private parseSubmissionDate(): DateTime {
+    let submissionDate: DateTime | undefined;
+    
+    console.log('Parsing submission date from GitHub context...')
+    switch(github.context.eventName) {
+      case 'workflow_dispatch':
+      case 'push':
+        // pushed_at is a timestamp for this commit
+        const pushed = github.context.payload.pushed_at;
+        if(pushed) {
+          submissionDate = DateTime.fromSeconds(parseInt(pushed))
+        }
+        break;
+      case 'release':
+        // created_at is an ISO date string for this release
+        const created = github.context.payload.release.created_at;
+        if(created) {
+          submissionDate = DateTime.fromISO(created)
+        }
+        break;
+    }
+
+    // check that we were able to parse the submission date
+    if(!submissionDate) {
+      throw new Error(`Could not parse submission date from GitHub context. Event name: ${github.context.eventName}`)
+    }
+    console.log(`\tSubmission date: ${submissionDate}`)
+    return submissionDate
+  }
+
+  /**
+   * Checks the given submission date and calculates the late penalty if applicable
+   */
+  private checkSubmissionDate(submittedDate: DateTime){
+    // TODO
   }
 }
 
